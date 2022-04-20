f35aed726fdecaf85b825e7d4699ab5392098656
Anatolii Charikov <pepsh6881@gmail.com>
2022-04-21 00:08:31 +0300

    update the file
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- dealing_with_todo_tasks.sh
+++ dealing_with_todo_tasks.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 DATA_FILE=??????
+TODAY_DATE=$(date "+%F")
+LAST_ACCESS_DATE_TO_DATA_FILE=$(grep LAST_ACCESS_DATE_TO_DATA_FILE $DATA_FILE | cut -d':' -f2)
 
+if [ "$LAST_ACCESS_DATE_TO_DATA_FILE" = "$TODAY_DATE" ]; then
+	exit 1;
+fi
 
 TODAY_DATE=$(date "+%F" | cut -d'-' -f2,3)
 NEXTDAY_DATE=$(date "+%F" --date="next day" | cut -d'-' -f2,3)
@@ -13,6 +18,8 @@ DONE_TASK_LABEL=x
 
 sed -i "s/^$DONE_TASK_LABEL:/$TODAY_TASK_LABEL:/g" $DATA_FILE
 sed -i "s/^$NEXT_DAY_TASK_LABEL:/$TODAY_TASK_LABEL:/g" $DATA_FILE
-
 sed -i "s/^$TODAY_DATE:/$TODAY_TASK_LABEL:/g" $DATA_FILE
 sed -i "s/^$NEXTDAY_DATE:/$NEXT_DAY_TASK_LABEL:/g" $DATA_FILE
+
+TODAY_DATE=$(date "+%F")
+sed -i "1s/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/$TODAY_DATE/" $DATA_FILE

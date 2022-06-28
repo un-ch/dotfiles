b300a82d1bf34bb9688a6106f5f05e4fece004b3
Anatolii Charikov <pepsh6881@gmail.com>
2022-06-28 21:34:23 +0300

    update the file
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- install_program.sh
+++ install_program.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-SOFT_LIST="eog \
+PROGRAM_LIST="eog \
+	vim \
 	gcc \
 	g++ \
+	nasm \
+	redshift \
+	geoclue-2.0 \
 	libncurses5-dev \
 	libncursesw5-dev \
+	qbittorrent \
+	texlive-base \
+	texlive-latex-base\
+	universal-ctags \
+	mplayer \
+	vlc \
+	atril \
+	evince \
+	gimp \
+	chromium \
+	firefox-esr \
 	"
-
-apt-get install -y $SOFT_LIST
+apt-get install  $PROGRAM_LIST

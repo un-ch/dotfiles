697ddaf59e6614ce2b78d20134e2dc7e2b04edc1
Anatolii Charikov <pepsh6881@gmail.com>
2022-11-05 22:14:30 +0200

    change the variable name
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- open_prj.sh
+++ open_prj.sh
@@ -4,7 +4,7 @@ if [ "$1" = "" ]; then
 	echo "error: no project name";
 fi
 
-WORK_DIR=/home/us/prj
+WORKING_DIRECTORY=
 
-/usr/bin/i3-msg workspace 2 1>/dev/null ; xterm -e "cd $WORK_DIR/$1; $SHELL" & sleep 1
-/usr/bin/i3-msg workspace 1 1>/dev/null ; xterm -e "cd $WORK_DIR/$1; $SHELL" & sleep 1;
+/usr/bin/i3-msg workspace 2 1>/dev/null ; xterm -e "cd $WORKING_DIRECTORY/$1; $SHELL" & sleep 1
+/usr/bin/i3-msg workspace 1 1>/dev/null ; xterm -e "cd $WORKING_DIRECTORY/$1; $SHELL" & sleep 1;

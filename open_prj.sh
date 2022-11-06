0bd5a8f4e29a83de5b3dae7af55208efccc43301
Anatolii Charikov <pepsh6881@gmail.com>
2022-11-06 18:48:08 +0200

    update the file
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- open_prj.sh
+++ open_prj.sh
@@ -7,11 +7,11 @@ fi
 
 WORKING_DIR=
 
-if [ ! -d "$WORK_DIR/$1" ]; then
+if [ ! -d "$WORKING_DIR/$1" ]; then
 	echo "error: $1 project is not found";
 	find $WORK_DIR -maxdepth 1 -name "*$1*" -type d -printf "%p\n";
 	exit;
 fi
 
 /usr/bin/i3-msg workspace 2 1>/dev/null ; xterm -e "cd $WORKING_DIR/$1; $SHELL" & sleep 1
-/usr/bin/i3-msg workspace 1 1>/dev/null ; xterm -e "cd $WORKING_DIR/$1; $SHELL" & sleep 1;
+/usr/bin/i3-msg workspace 1 1>/dev/null ; xterm -e "cd $WORKING_DIR/$1; $SHELL" &
